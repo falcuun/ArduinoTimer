f820742fcd078bbab19693b7bc02cced4455d478
falcuun <falcuun@gmail.com>
2019-01-03 00:03:23 +0100

    Added basic Timer Count down Logic With Printing to the LCD

--- ArduinoTimer.ino
+++ ArduinoTimer.ino
@@ -3,31 +3,30 @@
 #include <stdlib.h>
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; // Pins Used for LCD Display
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7); // LCD Display initialized with given pins
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);                  // LCD Display initialized with given pins
 
 const int lcdWidth = 16;
 const int lcdHeight = 2;
 
 const byte columnNumber = 4; // Number of Columns on Keypad
-const byte rowNumber    = 4; // Number of Rows on Keypad
+const byte rowNumber = 4;    // Number of Rows on Keypad
 
-char keys[rowNumber][columnNumber] = {  //=============
-  {'1','2','3', 'A'},                   //
-  {'4','5','6', 'B'},                   //  Keypad Keys Defined in Rows and Columns
-  {'7','8','9', 'C'},                   //    
-  {'#','0','*', 'D'}                    //
-};                                      //=============     
+char keys[rowNumber][columnNumber] = {
+                          //=============
+    {'1', '2', '3', 'A'}, //
+    {'4', '5', '6', 'B'}, //  Keypad Keys Defined in Rows and Columns
+    {'7', '8', '9', 'C'}, //
+    {'#', '0', '*', 'D'}  //
+};                        //=============
 
-byte rowPins[rowNumber] = {A1, A2, A3, A4}; // Keypad Columns Connection
-byte columnPins[columnNumber] = {9, 8, 7, 6}; // Keypad Rows Connection
+byte rowPins[rowNumber] = {9, 8, 7, 6};           // Keypad Columns Connection
+byte columnPins[columnNumber] = {A1, A2, A3, A4}; // Keypad Rows Connection
 
-Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, columnPins, rowNumber, columnNumber); // Construct Keypad with given parameters 
+Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, columnPins, rowNumber, columnNumber); // Construct Keypad with given parameters
 
-char minutes[2];
-char seconds[2];
-int minute = 0, second = 0;
-int index = 0;
-bool timerRunning;
+char minutes[3], seconds[3];           // Char Arrays to be turned into an int
+int minute = 0, second = 0, index = 0; // Variables holding values from char arrays
+bool timerRunning = true;              // State of the Timer, Running/Not Running
 
 void setup()
 {
@@ -38,28 +37,84 @@ void setup()
 void loop()
 {
     inputTime();
-    Serial.println(minutes);
 }
 
 void inputTime()
 {
+    lcd.blink();
     char c = myKeypad.getKey();
-    if(c)
+    if (c)
     {
-        switch(index)
+        switch (index)
         {
-            case 0: minutes[0] = c; break;
-            case 1: minutes[1] = c; break;
-            case 2: seconds[0] = c; break;
-            case 3: seconds[1] = c; break;
+        case 0:
+            minutes[0] = c;
+            lcd.setCursor(index, 0);
+            break;
+        case 1:
+            minutes[1] = c;
+            minutes[2] = '\0';
+            lcd.setCursor(index, 0);
+            break;
+        case 2:
+            seconds[0] = c;
+            lcd.setCursor(index, 0);
+            break;
+        case 3:
+            seconds[1] = c;
+            seconds[2] = '\0';
+            lcd.setCursor(index, 0);
+            break;
         }
         index++;
-        if(index == 4)
+        if (index == 4)
         {
             index = 0;
             lcd.setCursor(index, 0);
             minute = atoi(minutes);
             second = atoi(seconds);
+            runTimer();
         }
-    }  
+    }
+}
+
+void runTimer()
+{
+    while (timerRunning)
+    {
+      lcd.clear();
+        if (second <= 0)
+        {
+            if (minute <= 0)
+            {
+                timerRunning = false;
+            }
+            else
+            {
+                minute--;
+                second = 59;
+            }
+        }
+        else
+        {
+            second--;
+        }
+        printToLCD();
+        delay(1000);
+    }
+}
+
+void printToLCD()
+{
+    if (minute < 10)
+    {
+        lcd.print('0');
+    }
+    lcd.print(minute);
+    lcd.print(':');
+    if (second < 10)
+    {
+        lcd.print('0');
+    }
+    lcd.print(second);
 }
